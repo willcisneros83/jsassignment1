632fc58994ef9e2bb3ede2856fd427a08d21a6e9
willcisneros83 <92768239+willcisneros83@users.noreply.github.com>
2021-11-08 19:19:15 -0600

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,24 @@
+console.log("Script2")
+let userName="Will"
+let age=38
+let sex="M"
+let ethnicity="Hispanic"
+let hairColor="Black"
+let eyeColor="Brown"
+let numberChildren="2"
+let occupation="Sales"
+let favoriteSport="Football"
+let favoriteFood="Tacos"
+let favoriteGenre="Rock Music"
+let favoriteMovie="The Big Lebowski"
+let maritalStatus="Happily Divorced"
+let favoriteDestination="The Beach"
+let favoriteDrink="Coffee"
+let educationLevel="Bachelors Degree"
+let militaryStatus="Veteran"
+let militaryBranch="Army"
+let favoriteHobby="Concerts"
+
+document.write(`<h3> My name is ${userName} I am ${age} years old and my favorite hobby is going to ${favoriteHobby}. I also enjoy watching ${favoriteSport}, going to ${favoriteDestination} and listening to ${favoriteGenre}.</h3>`)
+
+console.log(`Name: ${userName}\nAge: ${age}\nSex: ${sex}\nEthnicity: ${ethnicity}\nHair Color: ${hairColor}\nEye Color: ${eyeColor}\nChildren: ${numberChildren}\nOccupation: ${occupation}\nFavorite Sport: ${favoriteSport}\nFavorite Food: ${favoriteFood}\nMusic: ${favoriteGenre}\nMovie: ${favoriteMovie}\nMarital Status: ${maritalStatus}\nFavorite Destination: ${favoriteDestination}\nFavorite Drink: ${favoriteDrink}\nEducation: ${educationLevel}\nMilitary Status: ${militaryStatus}\nBranch: ${militaryBranch}\nHobby: ${favoriteHobby}`)
